aceb59f6d685deb2b5f424e705bbda0aa1f96e5b
FalseBaconator <113189775+FalseBaconator@users.noreply.github.com>
2022-09-12 10:52:10 -0300

    Initial commit

---
+++ HelloWorldGitHub/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldGitHub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world but this time with more github.");
+            Console.ReadKey();
+        }
+    }
+}
